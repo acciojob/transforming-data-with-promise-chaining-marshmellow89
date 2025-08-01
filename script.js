c3436e0e7c31b6e391257e02a98154bd2f6f4cfc
marshmellow89 <pritamsingh100app@gmail.com>
2025-08-01 15:44:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,35 +1,43 @@
 //your JS code here. If required.
 const btn = document.querySelector("#btn");
-btn.addEventListener("onclick",showNumber());
+btn.addEventListener("click",showNumber());
 
 function showNumber(){
 const getDiv = document.querySelector("#output");
 const getInput = document.querySelector("#ip").value;
 	
 return new Promise(resolve => setTimeout(() => {
-	const toShow = resolve(`Result: ${getInput}`);
-	getDiv.textContent = toShow;
+	getDiv.textContent = `Result: ${getInput}`;
+	resolve(getInput);
 },2000))
 	.then(num =>{
+		return new Promise(resolve =>{
 		setTimeout(() =>{
 			getDiv.textContent =`Result: ${num*2}`;
 		},2000);
-	})
+	});
+			})
 	.then(num => {
+		return new Promise(resolve => {
 		setTimeout(() => {
 			getDiv.textContent =`Result: ${num-3}`;
 		},1000);
-	})
+	});
+			})
 	.then( num => {
+		return new Promise(resolve => {
 		setTimeout(() => {
 			getDiv.textContent =`Result: ${num/2}`;
 		},1000)
-	})
+	});
+		})
 	.then( num => {
+		return new Promise(resolve => {
 		setTimeout(() => {
 			getDiv.textContent =`Final Result: ${num+10}`;
 		},1000)
 	});
+		});
 
 	
 	
